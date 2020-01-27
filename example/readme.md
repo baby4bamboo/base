7114b83338a3a6b1a3055477a4f48e37ecc63e7a
Svan Yao (bayao) <bayao@cisco.com>
2020-01-27 23:36:17 +0800

    init

---
+++ example/readme.md
@@ -0,0 +1,2 @@
+题目:https://github.com/Yixiaohan/show-me-the-code
+
---
+++ python_test_100/8.py
@@ -0,0 +1,8 @@
+
+
+
+if __name__ == '__main__':
+    for i in range(1,10):
+        for j in range(1,10):
+            if j>=i:
+                print( str(i)+"*"+str(j)+"="+str(i*j) )
